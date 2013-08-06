LineageOS/android_frameworks_opt_telephony

Properly notify of precise call state change after connection hangup.
When disconnecting connections via Connection.hangup(), previously no
precise call state notification was sent out.
This change should fix BT headsets ringing indefinitely after a
blacklisted call came in, as BluetoothPhoneService can now update the
headset state properly.

Change-Id: I4439d7d8ed47e72da9d061b885ec9a0e6e1b784d

src/java/com/android/internal/telephony/cdma/CdmaCallTracker.java
@@ -485,6 +485,7 @@ public final class CdmaCallTracker extends CallTracker {
         Connection newRinging = null; //or waiting
         boolean hasNonHangupStateChanged = false;   // Any change besides
                                                     // a dropped connection
+        boolean hasAnyCallDisconnected = false;
         boolean needsPollDelay = false;
         boolean unknownConnectionAppeared = false;
 
@@ -675,14 +676,11 @@ public final class CdmaCallTracker extends CallTracker {
                     log("setting cause to " + cause);
                 }
                 mDroppedDuringPoll.remove(i);
-                conn.onDisconnect(cause);
-            } else if (conn.mCause == Connection.DisconnectCause.LOCAL) {
-                // Local hangup
+                hasAnyCallDisconnected |= conn.onDisconnect(cause);
+            } else if (conn.mCause == Connection.DisconnectCause.LOCAL
+                    || conn.mCause == Connection.DisconnectCause.INVALID_NUMBER) {
                 mDroppedDuringPoll.remove(i);
-                conn.onDisconnect(Connection.DisconnectCause.LOCAL);
-            } else if (conn.mCause == Connection.DisconnectCause.INVALID_NUMBER) {
-                mDroppedDuringPoll.remove(i);
-                conn.onDisconnect(Connection.DisconnectCause.INVALID_NUMBER);
+                hasAnyCallDisconnected |= conn.onDisconnect(conn.mCause);
             }
         }
 
@@ -701,7 +699,7 @@ public final class CdmaCallTracker extends CallTracker {
         // 1) the phone has started to ring
         // 2) A Call/Connection object has changed state...
         //    we may have switched or held or answered (but not hung up)
-        if (newRinging != null || hasNonHangupStateChanged) {
+        if (newRinging != null || hasNonHangupStateChanged || hasAnyCallDisconnected) {
             internalClearDisconnected();
         }
 
@@ -711,7 +709,7 @@ public final class CdmaCallTracker extends CallTracker {
             mPhone.notifyUnknownConnection();
         }
 
-        if (hasNonHangupStateChanged || newRinging != null) {
+        if (hasNonHangupStateChanged || newRinging != null || hasAnyCallDisconnected) {
             mPhone.notifyPreciseCallStateChanged();
         }
 

src/java/com/android/internal/telephony/cdma/CdmaConnection.java
@@ -439,8 +439,10 @@ public class CdmaConnection extends Connection {
     }
 
     /** Called when the radio indicates the connection has been disconnected */
-    /*package*/ void
+    /*package*/ boolean
     onDisconnect(DisconnectCause cause) {
+        boolean changed = false;
+
         mCause = cause;
 
         if (!mDisconnected) {
@@ -450,10 +452,11 @@ public class CdmaConnection extends Connection {
             mOwner.mPhone.notifyDisconnect(this);
 
             if (mParent != null) {
-                mParent.connectionDisconnected(this);
+                changed = mParent.connectionDisconnected(this);
             }
         }
         releaseWakeLock();
+        return changed;
     }
 
     /** Called when the call waiting connection has been hung up */

src/java/com/android/internal/telephony/gsm/GsmCallTracker.java
@@ -438,6 +438,7 @@ public final class GsmCallTracker extends CallTracker {
         Connection newRinging = null; //or waiting
         boolean hasNonHangupStateChanged = false;   // Any change besides
                                                     // a dropped connection
+        boolean hasAnyCallDisconnected = false;
         boolean needsPollDelay = false;
         boolean unknownConnectionAppeared = false;
 
@@ -605,15 +606,11 @@ public final class GsmCallTracker extends CallTracker {
                     log("setting cause to " + cause);
                 }
                 mDroppedDuringPoll.remove(i);
-                conn.onDisconnect(cause);
-            } else if (conn.mCause == Connection.DisconnectCause.LOCAL) {
-                // Local hangup
+                hasAnyCallDisconnected |= conn.onDisconnect(cause);
+            } else if (conn.mCause == Connection.DisconnectCause.LOCAL
+                    || conn.mCause == Connection.DisconnectCause.INVALID_NUMBER) {
                 mDroppedDuringPoll.remove(i);
-                conn.onDisconnect(Connection.DisconnectCause.LOCAL);
-            } else if (conn.mCause ==
-                Connection.DisconnectCause.INVALID_NUMBER) {
-                mDroppedDuringPoll.remove(i);
-                conn.onDisconnect(Connection.DisconnectCause.INVALID_NUMBER);
+                hasAnyCallDisconnected |= conn.onDisconnect(conn.mCause);
             }
         }
 
@@ -632,7 +629,7 @@ public final class GsmCallTracker extends CallTracker {
         // 1) the phone has started to ring
         // 2) A Call/Connection object has changed state...
         //    we may have switched or held or answered (but not hung up)
-        if (newRinging != null || hasNonHangupStateChanged) {
+        if (newRinging != null || hasNonHangupStateChanged || hasAnyCallDisconnected) {
             internalClearDisconnected();
         }
 
@@ -642,7 +639,7 @@ public final class GsmCallTracker extends CallTracker {
             mPhone.notifyUnknownConnection();
         }
 
-        if (hasNonHangupStateChanged || newRinging != null) {
+        if (hasNonHangupStateChanged || newRinging != null || hasAnyCallDisconnected) {
             mPhone.notifyPreciseCallStateChanged();
         }
 

src/java/com/android/internal/telephony/gsm/GsmConnection.java
@@ -406,8 +406,10 @@ public class GsmConnection extends Connection {
     }
 
     /** Called when the radio indicates the connection has been disconnected */
-    /*package*/ void
+    /*package*/ boolean
     onDisconnect(DisconnectCause cause) {
+        boolean changed = false;
+
         mCause = cause;
 
         if (!mDisconnected) {
@@ -422,10 +424,11 @@ public class GsmConnection extends Connection {
             mOwner.mPhone.notifyDisconnect(this);
 
             if (mParent != null) {
-                mParent.connectionDisconnected(this);
+                changed = mParent.connectionDisconnected(this);
             }
         }
         releaseWakeLock();
+        return changed;
     }
 
     // Returns true if state has changed, false if nothing changed

marlontoe/frameworks_opt_telephony

Change-Id: I299650e2dcead036f323b85759064cd75c589dc1

src/java/com/android/internal/telephony/cdma/CdmaCallTracker.java
@@ -497,6 +497,7 @@ public final class CdmaCallTracker extends CallTracker {
         Connection newRinging = null; //or waiting
         boolean hasNonHangupStateChanged = false;   // Any change besides
                                                     // a dropped connection
+        boolean hasAnyCallDisconnected = false;
         boolean needsPollDelay = false;
         boolean unknownConnectionAppeared = false;
 
@@ -687,14 +688,11 @@ public final class CdmaCallTracker extends CallTracker {
                     log("setting cause to " + cause);
                 }
                 droppedDuringPoll.remove(i);
-                conn.onDisconnect(cause);
-            } else if (conn.cause == Connection.DisconnectCause.LOCAL) {
-                // Local hangup
+                hasAnyCallDisconnected |= conn.onDisconnect(cause);
+            } else if (conn.cause == Connection.DisconnectCause.LOCAL
+                    || conn.cause == Connection.DisconnectCause.INVALID_NUMBER) {
                 droppedDuringPoll.remove(i);
-                conn.onDisconnect(Connection.DisconnectCause.LOCAL);
-            } else if (conn.cause == Connection.DisconnectCause.INVALID_NUMBER) {
-                droppedDuringPoll.remove(i);
-                conn.onDisconnect(Connection.DisconnectCause.INVALID_NUMBER);
+                hasAnyCallDisconnected |= conn.onDisconnect(conn.cause);
             }
         }
 
@@ -713,7 +711,7 @@ public final class CdmaCallTracker extends CallTracker {
         // 1) the phone has started to ring
         // 2) A Call/Connection object has changed state...
         //    we may have switched or held or answered (but not hung up)
-        if (newRinging != null || hasNonHangupStateChanged) {
+        if (newRinging != null || hasNonHangupStateChanged || hasAnyCallDisconnected) {
             internalClearDisconnected();
         }
 
@@ -723,7 +721,7 @@ public final class CdmaCallTracker extends CallTracker {
             phone.notifyUnknownConnection();
         }
 
-        if (hasNonHangupStateChanged || newRinging != null) {
+        if (hasNonHangupStateChanged || newRinging != null || hasAnyCallDisconnected) {
             phone.notifyPreciseCallStateChanged();
         }
 

src/java/com/android/internal/telephony/cdma/CdmaConnection.java
@@ -446,8 +446,9 @@ public class CdmaConnection extends Connection {
     }
 
     /** Called when the radio indicates the connection has been disconnected */
-    /*package*/ void
+    /*package*/ boolean
     onDisconnect(DisconnectCause cause) {
+        boolean changed = false;
         this.cause = cause;
 
         if (!disconnected) {
@@ -458,10 +459,11 @@ public class CdmaConnection extends Connection {
             owner.phone.notifyDisconnect(this);
 
             if (parent != null) {
-                parent.connectionDisconnected(this);
+                changed = parent.connectionDisconnected(this);
             }
         }
         releaseWakeLock();
+        return changed;
     }
 
     /** Called when the call waiting connection has been hung up */

src/java/com/android/internal/telephony/gsm/GsmCallTracker.java
@@ -432,6 +432,7 @@ public final class GsmCallTracker extends CallTracker {
         Connection newRinging = null; //or waiting
         boolean hasNonHangupStateChanged = false;   // Any change besides
                                                     // a dropped connection
+        boolean hasAnyCallDisconnected = false;
         boolean needsPollDelay = false;
         boolean unknownConnectionAppeared = false;
 
@@ -595,15 +596,11 @@ public final class GsmCallTracker extends CallTracker {
                     log("setting cause to " + cause);
                 }
                 droppedDuringPoll.remove(i);
-                conn.onDisconnect(cause);
-            } else if (conn.cause == Connection.DisconnectCause.LOCAL) {
-                // Local hangup
+                hasAnyCallDisconnected |= conn.onDisconnect(cause);
+            } else if (conn.cause == Connection.DisconnectCause.LOCAL
+                    || conn.cause == Connection.DisconnectCause.INVALID_NUMBER) {
                 droppedDuringPoll.remove(i);
-                conn.onDisconnect(Connection.DisconnectCause.LOCAL);
-            } else if (conn.cause ==
-                Connection.DisconnectCause.INVALID_NUMBER) {
-                droppedDuringPoll.remove(i);
-                conn.onDisconnect(Connection.DisconnectCause.INVALID_NUMBER);
+                hasAnyCallDisconnected |= conn.onDisconnect(conn.cause);
             }
         }
 
@@ -622,7 +619,7 @@ public final class GsmCallTracker extends CallTracker {
         // 1) the phone has started to ring
         // 2) A Call/Connection object has changed state...
         //    we may have switched or held or answered (but not hung up)
-        if (newRinging != null || hasNonHangupStateChanged) {
+        if (newRinging != null || hasNonHangupStateChanged || hasAnyCallDisconnected) {
             internalClearDisconnected();
         }
 
@@ -632,7 +629,7 @@ public final class GsmCallTracker extends CallTracker {
             phone.notifyUnknownConnection();
         }
 
-        if (hasNonHangupStateChanged || newRinging != null) {
+        if (hasNonHangupStateChanged || newRinging != null || hasAnyCallDisconnected) {
             phone.notifyPreciseCallStateChanged();
         }
 

src/java/com/android/internal/telephony/gsm/GsmConnection.java
@@ -411,8 +411,9 @@ public class GsmConnection extends Connection {
     }
 
     /** Called when the radio indicates the connection has been disconnected */
-    /*package*/ void
+    /*package*/ boolean
     onDisconnect(DisconnectCause cause) {
+        boolean changed = false;
         this.cause = cause;
 
         if (!disconnected) {
@@ -428,10 +429,11 @@ public class GsmConnection extends Connection {
             owner.phone.notifyDisconnect(this);
 
             if (parent != null) {
-                parent.connectionDisconnected(this);
+                changed = parent.connectionDisconnected(this);
             }
         }
         releaseWakeLock();
+        return changed;
     }
 
     // Returns true if state has changed, false if nothing changed